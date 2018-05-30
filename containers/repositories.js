970501d77c4d9dec66fd34a71539ce8f25143b73
sebashack <sebastian0092@gmail.com>
2018-05-30 04:05:25 -0500

    Displaying repos in view

--- containers/repositories.js
+++ containers/repositories.js
@@ -3,8 +3,22 @@ import { connect } from "react-redux";
 import Repositories from "../components/repositories";
 import { loadRepos } from "../actions";
 
+const getNameAndLanguage = obj => {
+  return {
+    name: obj.name,
+    language: obj.language
+  };
+};
+
 const mapStateToProps = ({ waiting, response, error }) => {
-  return { waiting, response, error };
+  const data = response.data;
+  const simplifiedRepos = !data ? null : data.map(getNameAndLanguage);
+
+  return {
+    waiting,
+    response: { status: response. status, data: simplifiedRepos },
+    error
+  };
 };
 
 const mapDispatchToProps = dispatch => {
