32ee18c0f887a085389ae598154f237c4fb5f64d
sebashack <sebastian0092@gmail.com>
2018-05-30 04:09:58 -0500

    Displaying name and language in the same line

--- components/repository.js
+++ components/repository.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
-import * as NB from "native-base";
 
 const Repository = ({repo}) => (
-  <NB.ListItem>
-    <Text>{repo.name} - {repo.language}</Text>
-  </NB.ListItem>
+  <View>
+    <Text>{`${repo.name} --- ${repo.language}`}</Text>
+  </View>
 );
 
 export default Repository;
